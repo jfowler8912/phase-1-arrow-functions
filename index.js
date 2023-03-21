3736222e79d6bb9f6a601d03a227fdbe4d88c4fe
JoshuaFowler <jfowler8912@gmail.com>
2023-03-20 20:07:32 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function divide(num1, num2) {
+    return num1 / num2
+}
+const square = (parameter1) => parameter1**2;
+square(2)
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
